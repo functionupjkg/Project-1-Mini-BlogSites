effd2e60c896c0212df2771df9bab2a03c2dfe98
Madhurilenka <madhurilenka98@gmail.com>
2022-09-05 19:14:32 +0530

    updated by madhu

--- src/controller/blogsController.js
+++ src/controller/blogsController.js
@@ -2,6 +2,20 @@ const authorModel = require("../Model/authorModel")
 
 const blogsModel = require("../Model/blogsModel")
 
+const jwt = require('jsonwebtoken');
+
+
+
+// --------------------Create Blogs Function ------------------------------------
+const createBlogs = async function (req, res){
+    let data = req.body
+    let saveData = await blogsModel.create(data)
+    res.status(201).send({status : true, msg: "Blogs created successfully", data:saveData});
+    
+}
+
+//-------------get===============//
+
 const getblog = async function (req, res) {
     let data = req.query
     let deleted = data.isdeleted
@@ -22,5 +36,8 @@ const getblog = async function (req, res) {
         return res.status(401).send({ status: false, msg: "no document found" })
     }
 }
+
+module.exports.createBlogs=createBlogs;
+
 
 module.exports.getblog = getblog;
